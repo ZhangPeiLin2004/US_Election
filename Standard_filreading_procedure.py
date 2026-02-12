328671fc09a53c53d09aaee8b9f22050e4c9db3a
ZhangPeiLin2004 <peilin.zhang8888@gmail.com>
2026-02-12 20:29:43 +0800

    Add files via upload

--- Standard_filreading_procedure.py
+++ Standard_filreading_procedure.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
+# Read a small chunk
 df = pd.read_csv("subjects_AND_sampling_metadata_anonymized_full.csv", chunksize=100)
-for chunk in df:
-    print(chunk.head())
-    break
+chunk = next(df)
+
+print(chunk.columns.tolist())
+
+pd.set_option('display.max_columns', None)
+print(chunk.head())
+
+print(chunk.dtypes)
